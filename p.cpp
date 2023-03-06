8372f97058b15ba0492ea73d3e78c10621853fdd
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-03-06 15:49:56 +0700

    first commit

---
+++ p.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+   string combo, aaa;
+
+   cout<<"masukkan combo magic chess mu = ";
+   getline(cin, combo);
+
+    aaa = "gunner" ? "jago " : "cupu";
+
+    cout<<"lu "<<aaa;
+
+
+
+}
